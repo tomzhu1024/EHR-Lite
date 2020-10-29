7fe0014fe69e00b72508e1794ab6a3e691ef7208
sheldonchen <nh8157@hotmail.com>
2020-10-29 20:26:06 +0800

    ds store altered

---
+++ front/src/js/components/counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return (
+            <h3>Count</h3>
+        );
+    }
+}
+ 
+export default Counter;
--- front/src/js/index.jsx
+++ front/src/js/index.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import {Button} from 'antd';
 import '../css/index.less';
+import Counter from './components/counter.jsx'
 
 const App = () => {
     return (
-        <>
+        <React.Fragment>
             <h1>Hello World</h1>
             <p>This is a test page.</p>
+            <Counter />
             <Button onClick={() => {
                 alert('You clicked the button.');
             }}>Click Me</Button>
-        </>
+        </React.Fragment>
     );
 };
 
